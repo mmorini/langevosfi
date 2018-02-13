255ac0f6adc7b489d5eb9c996ba525ca80581bc0
Artemy Kolchinsky <artemyk@gmail.com>
2018-02-13 14:05:17 -0700

    Update

--- model.py
+++ model.py
@@ -8,13 +8,19 @@ from utils import log
 from mutators import ProbitVectorGaussian
 
 # ********** Helper functions ***********
+def sanity_check_grammars(meme_probs, grammars):
+    for g in grammars:
+        assert(np.all( g >= 0))
+        assert(np.isclose( g.sum(), 1))
+        assert(np.allclose( g.sum(axis=1), meme_probs ))
 
-# Normalize non-negative matrix to be a joint probability matrix with fixed meme_probs
+# Normalize non-negative matrix to be a joint probability matrix with 
+# fixed marginals meme_probs
 def normalize(meme_probs, p):
-    r = p * (meme_probs/p.sum(axis=1)[:,None])
+    r = p * (meme_probs/p.sum(axis=1))[:,None]
     return r
 
-# Function to initalize random grammars.  Meme marginals are set to meme_probs
+# Function to initalize random grammars. Meme marginals are fixed to meme_probs
 def init_grammars(meme_probs, num_agents, num_memes, num_lexes):
     # Initialize agents with random grammars
     grammars = []
@@ -25,21 +31,24 @@ def init_grammars(meme_probs, num_agents, num_memes, num_lexes):
     return grammars
 
 
-# Functions for measuring comprehension (i.e., 1-probability of error) between two grammars
-# Each grammar is an [num_memes X num_lexes] numpy array
+# Functions for measuring comprehension (i.e., 1-probability of error) between 
+# two grammars. Each grammar is an [num_memes X num_lexes] numpy array
 def get_comprehension(speaker_grammar, listener_grammar):
-    # Takes in two grammar arrays, returns a positive number indicating comprehension
+    # Takes in two grammar arrays, returns a number indicating comprehension
     listener_meme_given_lexe = listener_grammar/(listener_grammar.sum(axis=0) + 1e-16)
     
-    # Comprehension is given by Trace( speaker_grammar.dot(listener_meme_given_lexe^T) )
-    # Turns out this is equal to sum( speaker_grammar * listener_meme_given_lexe )
-    #  where * is entrywise multiplication. This latter expression is faster to compute
+    # Comprehension is given by 
+    #    Trace( speaker_grammar.dot(listener_meme_given_lexe^T) )
+    # In fact, this is equal to 
+    #    sum( speaker_grammar * listener_meme_given_lexe )
+    # where * is element-wise multiplication.
+    # We use this latter expression, its faster
     return (speaker_grammar*listener_meme_given_lexe).sum()
 
 def get_comprehension_multiple_speakers(speaker_grammars, listener_grammar):
     # Same as calculate_comprehension, but for a list of speaker grammars 
-    #  (faster that calling get_comprehension many times, since listener_meme_given_lexe 
-    #   is only computed once)
+    #  (faster that calling get_comprehension many times, since
+    #   listener_meme_given_lexe is only computed once)
     listener_meme_given_lexe = listener_grammar/(listener_grammar.sum(axis=0) + 1e-16)
     comprehension_vals = []
     for sg in speaker_grammars:
@@ -47,62 +56,107 @@ def get_comprehension_multiple_speakers(speaker_grammars, listener_grammar):
     return comprehension_vals
 
 def get_comprehension_matrix(grammars):
-    # Calculate matrix of comprehension rates between each pair of agents
-    # TODO: Double check that this doesn't just measure comprehension 'one-way' 
-    #    (i.e., each agent is either speaker or receiver). It should do both ways
+    # Calculate matrix M of comprehension rates between each pair of agents
+    # M[a,b] is comprehension when a is speaker and b is listener
     num_agents = len(grammars)
     comprehension_matrix = np.zeros((num_agents,num_agents))
     for i in range(num_agents):
         comprehension_matrix[:,i] += get_comprehension_multiple_speakers(grammars, grammars[i])
     return comprehension_matrix
 
+# Calculate statistics on the population of grammars
+def get_grammars_stats(grammars_tensor, old_grammars_tensor):
+    # grammars_tensor is [num_memes, num_lexes, num_agents] np.array for current population
+    # old_grammars_tensor is same, but for populating during last reporting interval
+
+    stats = {}
+    grammars =[grammars_tensor[:,:,i] for i in range(grammars_tensor.shape[2])]
+    stats['Comprehension'] = get_comprehension_matrix(grammars).mean()
+
+    mean_grammar = grammars_tensor.mean(axis=2)
+    stats['GrammarVar'] = np.linalg.norm(grammars_tensor-mean_grammar[:,:,None])
+
+    if old_grammars_tensor is not None:
+        stats['AgentGrammarDrift'] = \
+          np.linalg.norm(grammars_tensor - old_grammars_tensor)
+        stats['MeanGrammarDrift'] = \
+          np.linalg.norm(mean_grammar-old_grammars_tensor.mean(axis=2))
+    else:
+        stats['AgentGrammarDrift'] = np.nan
+        stats['MeanGrammarDrift'] = np.nan
+
+    return stats
+
 
 # ****************** Main simulation function *********************************
-def run_simulation(grammars,     # List of num_agents numpy arrays, each one having shape [num_memes x num_steps]
-                   meme_probs,   # List of meme probabilities, [num_memes] numpy array
-                   num_agents,   # Number of agents 
-                   num_memes,    # Number of memes
-                   num_lexes,    # Number of lexes
-                   num_steps,    # Num_steps
-                   mutator_class=ProbitVectorGaussian,   # Mutation operator class
-                   mutation_scale=None,  # Scale of mutations to be used by mutation operator
-                   report_every=10000,   # How often to print stats
-                   logfile=None,         # Filename to which to print stats. None for stdout only
-                   temperature=0         # Temperature (used to decide acceptance)
-                   ):   
-    
+def run_simulation(
+    grammars,       # List of num_agents numpy arrays, 
+                    #   each having shape [num_memes x num_steps]
+    meme_probs,     # Numpy array with (fixed) meme probabilities
+    num_agents,     # Number of agents 
+    num_memes,      # Number of memes
+    num_lexes,      # Number of lexes
+    num_steps,      # Num_steps
+    mutator_class=ProbitVectorGaussian, # Mutation operator class
+    mutation_scale=None,  # Scale of mutations to be used by mutation operator
+    report_every=10000,   # How often to print stats
+    logfile=None,         # Filename to save stats. None means stdout only
+    temperature=0         # Temperature (used to decide acceptance)
+    ):   
     
     start_time = time.time()
 
     return_data = []  # Save stats in here and return from function
 
     # Cache random numbers, so we don't have to call this every time (faster)
-    speakers  = rand_ints(num_agents, num_steps)
-    listeners = rand_ints(num_agents, num_steps)
-    mutatememes = rand_ints(num_memes, num_steps) # TODO, sample with meme probs
-    mutatelexes = rand_ints(num_lexes, num_steps)
+    speakers    = rand_ints(num_agents, num_steps)
+    listeners   = rand_ints(num_agents, num_steps)
+    mutatememes = np.random.choice(num_memes, size=num_steps, p=meme_probs)
     acceptanceprobs = np.random.random(num_steps)
 
-    acceptedsteps = 0   # How many steps were accepted in the last report_every interval
-    takensteps    = 0   # How many steps were taken in the last report_every interval
-                        #  (this discounts the steps that were skipped when, by chance, speaker == listener
-    
-    log("Step Comprehension AcceptanceRate Time", logfile)
+    acceptedsteps = 0   # num. steps accepted in the last report_every interval
+    takensteps    = 0   # num. steps taken in the last report_every interval
+                        #  (doesn't count skipped steps w/ speaker == listener)
+    old_grammars_tensor = None  # Saved grammars from last reporting interval
+
+    log("Step Comprehension AcceptanceRate GrammarVar AgentGrammarDrift MeanGrammarDrift Time", logfile)
+    logstring = "%(Step)d %(Comprehension)0.8f %(AcceptanceRate)0.4f " + \
+                "%(GrammarVar)0.4f %(AgentGrammarDrift)0.4f " + \
+                "%(MeanGrammarDrift)0.4f %(Time)ds"
+    # Step is iteration number of the simulation
+    # Comprehension is average comprehension
+    # AcceptanceRate is proportion of steps accepted in last reporting interval
+    # GrammarVar is variance of grammars around mean grammar
+    # AgentGrammarDrift is L2 norm of [\Delta grammar] (over report interval),
+    #    for all agents
+    # MeanGrammarDrift is L2 norm of \Delta meangrammar (over report interval)
+    # Time is total elapsed time in seconds
 
     mutation_op = mutator_class(mutation_scale=mutation_scale)
 
     for step in range(num_steps):
         if step % report_every == (report_every-1):  # Print logs
-            mean_comprehension = get_comprehension_matrix(grammars).mean() 
-            mean_acceptance    = acceptedsteps/takensteps
-            log('%d %0.8f %0.4f %ds' % (step, mean_comprehension, mean_acceptance, time.time() - start_time), logfile)
-            return_data.append(
-                {'step'          : step, 
-                 'comprehension' : mean_comprehension, 
-                 'acceptance'    : mean_acceptance, 
-                 'time': time.time() - start_time})
+
+            # TODO: Possibly remove to increase performance. However, this 
+            # function should already be relatively fast
+            sanity_check_grammars(meme_probs, grammars)  
+
+            stats = {}  # Dictionary storing current stats
+            stats['Step'] = step
+            stats['Time'] = time.time() - start_time
+            stats['AcceptanceRate'] = acceptedsteps/float(takensteps)
+
+            grammars_tensor = np.stack(grammars, axis=2)
+            # Calculate various stats for current grammar population and add to stats dict
+            stats.update(get_grammars_stats(grammars_tensor, old_grammars_tensor))
+
+            log(logstring % stats, logfile)
+            return_data.append(stats)
+
             acceptedsteps = 0
             takensteps    = 0
+            old_grammars_tensor = grammars_tensor
+
 
         speaker  = speakers[step]
         listener = listeners[step]
@@ -111,22 +165,23 @@ def run_simulation(grammars,     # List of num_agents numpy arrays, each one hav
          
         current_meme = mutatememes[step]  # Meme to mutate/communicate
 
-        # Note that mutation_op.mutate requires a 2d numpy array, of shape [anything x num_lexes]
-        # In this case, we pass it a [1 x num_lexes] array
+        # Note that mutation_op.mutate requires a 2d numpy array, having
+        # shape [anything x num_lexes]. Here we pass in a [1 x num_lexes] array
         current_meme_lexe_probs = grammars[speaker][[current_meme,],:]
         new_probs = mutation_op.mutate(current_meme_lexe_probs)
 
         # Calculate change in comprehension
         listener_grammar = grammars[listener]
 
-        # Calculate conditional probabilities  of current memes, given all lexes
+        # Calculate conditional probabilities of current memes, given all lexes
         listener_meme_given_lexe = (listener_grammar[current_meme,:]/(listener_grammar.sum(axis=0) + 1e-16))
         
-        # TODO: double check that this will give correct change in comprehension
+        # TODO: double check that this gives correct change in comprehension
         change_in_comprehension = (new_probs - current_meme_lexe_probs).dot(listener_meme_given_lexe)
         
         # TODO: here is where we can insert model A, by doing something like
-        # np.sum( (new_probs - current_meme_lexe_probs) * (random_interaction_vector) * listener_meme_given_lexe)
+        # np.sum( (new_probs - current_meme_lexe_probs) * 
+        #           (random_interaction_vector) * listener_meme_given_lexe)
 
         if change_in_comprehension >= 0:
             accept_move = True
--- run.py
+++ run.py
@@ -16,6 +16,8 @@ parser.add_argument("--num_agents", type=int, help="Number of agents", default=1
 parser.add_argument("--num_memes" , type=int, help="Number of memes", default=10)
 parser.add_argument("--num_lexes" , type=int, help="Number of lexes", default=10)
 parser.add_argument("--num_steps" , type=int, help="Number of steps", default=1000000)
+parser.add_argument("--init_meme_prob", type=str, choices=['UNIFORM','RANDOM'], 
+                    help="How to initialize meme probabilities", default='UNIFORM')
 parser.add_argument("--mutator_class", type=str, choices=mutatorclassnames, help="Mutation operator", default=mutatorclassnames[0])
 parser.add_argument("--mutation_scale" , type=float, help="Mutation scale", default=0.1)
 parser.add_argument("--temperature", type=float, help="Temperature for acceptance step", default=0)
@@ -25,8 +27,11 @@ parser.add_argument("--logfile", type=str, help="Output file to log to")
 args = parser.parse_args()
 
 # Generate probability of memes (this will be fixed)
-meme_probs = np.ones(args.num_memes)    # can be replaced w/ memeprobs = np.random.rand(num_memes)
-meme_probs /= meme_probs.sum()
+if args.init_meme_prob == 'UNIFORM':
+  meme_probs = np.ones(args.num_memes) / args.num_memes
+elif args.init_meme_prob == 'RANDOM':
+  meme_probs = np.random.random(args.num_memes)
+  meme_probs /= meme_probs.sum()
 
 grammars = model.init_grammars(meme_probs, 
                                num_agents = args.num_agents, 
