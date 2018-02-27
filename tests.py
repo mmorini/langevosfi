fb29b68e25402f2f3f1e5120530758157020d819
Artemy Kolchinsky <artemyk@gmail.com>
2018-02-26 23:02:20 -0700

    Added a test

--- tests.py
+++ tests.py
@@ -6,13 +6,13 @@ def _call_run(**pargs):
 	args = dict(num_agents=5, num_memes=5, 
 		num_lexes=5, mutation_scale=0.1, num_steps=300, report_every=100)
 	args.update(**pargs)
-	print(args)
-	meme_probs = np.random.random(args['num_memes'])
-	meme_probs /= meme_probs.sum()
+	if 'meme_probs' not in args:
+		meme_probs = np.random.random(args['num_memes'])
+		args['meme_probs'] = meme_probs / meme_probs.sum()
 	grammars = model.init_grammars(num_agents=args['num_agents'],
 		num_lexes=args['num_lexes'], num_memes=args['num_memes'],
-		meme_probs=meme_probs)
-	model.run_simulation(grammars=grammars, meme_probs=meme_probs, **args)
+		meme_probs=args['meme_probs'])
+	model.run_simulation(grammars=grammars, **args)
 
 def test_run_simulation():
 	# Square grammars
@@ -29,4 +29,21 @@ def test_run_simulation_report_levels():
 	_call_run(report_level=1)
 	_call_run(report_level=2)
 	_call_run(report_level=3)
-	
+
+def test_run_simulation_comprehension_stats():
+	grammars = [np.eye(10)/10.,]*10
+	args = dict( num_agents=10,  num_memes=10, num_lexes=10, 
+		meme_probs=np.ones(10)/10., num_steps=1, report_every=1,
+		mutation_scale=0.1)
+	stats, _ = model.run_simulation(grammars=grammars, **args)
+	assert(len(stats)==1)
+	assert(np.isclose(stats[0]['Comprehension'], 1.))
+	assert(np.isclose(stats[0]['GrammarVar'], 0.))
+	for i in range(len(grammars)):
+	    grammars[i][:] = 0
+	    grammars[i][:,0]=0.1
+	stats, _ = model.run_simulation(grammars, **args)
+	assert(len(stats)==1)
+	assert(np.isclose(stats[0]['Comprehension'], 0.1))
+	assert(np.isclose(stats[0]['GrammarVar'], 0.))
+
