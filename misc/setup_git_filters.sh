d102736aaceb65f0dce718b04661c18f0069b3df
Artemy Kolchinsky <artemyk@gmail.com>
2018-02-27 13:02:26 -0700

    Trying to fix nbstripout

--- misc/setup_git_filters.sh
+++ misc/setup_git_filters.sh
@@ -1,3 +1,4 @@
-git config filter.stripoutput.clean "$(git rev-parse --show-toplevel)/misc/nbstripout.py"
+#git config filter.stripoutput.clean '$(git rev-parse --show-toplevel)/misc/nbstripout.py'
+git config filter.stripoutput.clean nbstripout
 git config filter.stripoutput.smudge cat
 git config filter.stripoutput.required true
