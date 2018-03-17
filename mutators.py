6f7c1380459143d476865e4fd8c1a2424a3b5600
hgiaffar <hgiaffar@cshl.edu>
2018-03-17 18:46:10 -0400

    Updated list of mutation operators to try out

--- mutators.py
+++ mutators.py
@@ -1,6 +1,9 @@
 from __future__ import print_function
 import numpy as np
+import scipy.special as spys
+import scipy.stats as scps
 from utils import probitperturb, rand_ints
+from config import git_strings
 
 class Mutator(object):
     # Base class for mutation operators
@@ -8,14 +11,14 @@ class Mutator(object):
         if mutation_scale is None:
             raise Exception('Must specify mutation_scale')
         self.mutation_scale = mutation_scale
-
+    
     def mutate(self, probs):
         # probs is a [_ X num_lexes] numpy array of probabilities to perturb
         saved_sums = probs.sum(axis=1)
         newprobs   = self._mutate(probs)
         new_sums   = newprobs.sum(axis=1)
         return newprobs * (saved_sums / new_sums)
-
+    
     def _mutate(self, probs):
         # probs is a [_ X num_lexes] numpy array of probabilities to perturb
         # Called by mutate. This method does mutation without normalization,
@@ -62,10 +65,128 @@ class AdditiveSingleUniform(Mutator):
         probs[:,lexes] += self.mutation_scale * np.random.rand(probs.shape[0])
         return probs
 
+
 class NoMutation(Mutator):
     def _mutate(self, probs):
         return probs
 
+
+# New mutaton operators, split by class (DW/AK)
+
+def clip(x):
+    # rectifying nonlinearity
+    return np.maximum(x,0)
+
+def clip_interval(x):
+    # clips to interval [0,1]
+    return np.minimum(np.maximum(x,0),1)
+
+
+# CLASS Ia           % mutators where f is not just a sum, non clipped
+
+class SingleBeta(Mutator):
+    # Draw new probability from beta distribution. Set variance to g/1e5 and solve for alpha and beta.
+    def _mutate(self, probs):
+        g = self.mutation_scale/1e5
+        lexes = rand_ints(probs.shape[1], probs.shape[0])
+        pd = probs[:,lexes]
+        alpha = np.abs((1/g)*(pd**2 - pd**3) - pd) + 1e-16
+        beta1 = np.abs((1/g)*(pd**3- 2*(pd**2) + pd) + pd) + 1e-16
+        probs = probs.copy()
+        probs[:,lexes] = np.random.beta(alpha, beta1, None)
+        return probs
+
+class VectorDirichlet(Mutator):
+    # Draw joint p from a dirichlet distribution with conc. vector alpha defined via probs
+    def _mutate(self, probs):
+        return np.random.dirichlet(probs[0,:]*100000 + 1e-16, 1)
+
+
+# CLASS Ib           % mutators where f is not just a sum, clipped
+
+class SingleGamma(Mutator):
+    # Draw mutation from a gamma distribution
+    def _mutate(self, probs):
+        lexes = rand_ints(probs.shape[1], probs.shape[0])
+        # set probs[:,lexes] as the mode of the gamma distribution
+        alpha = probs[:,lexes] + 1
+        probs = probs.copy()
+        probs[:,lexes] = clip_interval(self.mutation_scale * scps.gamma.rvs(alpha, scale=1, size=1))
+        return probs
+
+class VectorLogMultivariateGamma(Mutator):
+    # Associated vector form of gamma distibution
+    def _mutate(self, probs):
+        U = self.mutation_scale * abs(spys.multigammaln((probs[0,:] + 1 + 1e-16), 1))
+        probs = clip_interval(np.reshape(U, (1, U.shape[0])))
+        return probs
+
+
+# CLASS II           % mutators that can increase or decrease probability
+
+class AdditiveSingleClipExppGaussian(Mutator):
+    # additive perturbation with standard normal and clipping
+    def _mutate(self, probs):
+        lexes = rand_ints(probs.shape[1], probs.shape[0])
+        probs = probs.copy()
+        probs[:,lexes] += clip(self.mutation_scale * (np.exp(probs[:,lexes]) * np.random.randn(probs.shape[0])))
+        return probs
+
+class AdditiveVectorClipExppGaussian(Mutator):
+    # additive perturbation with uniform(0,var) to cond. prob p(lexe|meme)
+    def _mutate(self, probs):
+        return clip(probs + self.mutation_scale * (np.exp(probs) * np.random.randn(probs.shape[1])))
+
+
+class AdditiveSingleAbsExppGaussian(Mutator):
+    # additive perturbation with standard normal and clipping
+    def _mutate(self, probs):
+        lexes = rand_ints(probs.shape[1], probs.shape[0])
+        probs = probs.copy()
+        probs[:,lexes] += self.mutation_scale * (np.exp(probs[:,lexes]) * np.random.randn(probs.shape[0]))
+        return np.abs(probs)
+
+class AdditiveVectorAbsExppGaussian(Mutator):
+    # additive perturbation with uniform(0,var) to cond. prob p(lexe|meme)
+    def _mutate(self, probs):
+        return np.abs(probs + self.mutation_scale * np.exp(probs) * np.random.randn(probs.shape[1]))
+
+
+
+# CLASS III           % mutators that only increase probability
+
+class AdditiveSingleExppUniform(Mutator):
+    # additive perturbation with standard normal and clipping
+    def _mutate(self, probs):
+        lexes = rand_ints(probs.shape[1], probs.shape[0])
+        probs = probs.copy()
+        probs[:,lexes] += self.mutation_scale * (np.exp(probs[:,lexes]) * np.random.rand(probs.shape[0]))
+        return probs
+
+class AdditiveVectorExppUniform(Mutator):
+    # additive perturbation with uniform(0,var) to cond. prob p(lexe|meme)
+    def _mutate(self, probs):
+        return probs + self.mutation_scale * (np.exp(probs) * np.random.random(probs.shape))
+
+
+class ArcsSingleClip(Mutator):
+    # Tanmoy's suggested mutation operator
+    def _mutate(self,probs):
+        lexes = rand_ints(probs.shape[1], probs.shape[0])
+        probs = probs.copy()
+        probs[:,lexes] = 0.5 * (np.sin(np.arcsin(2*probs[:,lexes]-1) + self.mutation_scale) + 1)
+        return clip(probs)
+
+class ArcsVectorClip(Mutator):
+    # Tanmoy's suggested mutation operator: vector
+    def _mutate(self, probs):
+        return clip(0.5 * abs(np.sin(np.arcsin(2*probs-1) + self.mutation_scale) + 1))
+
+
+
 VALID_MUTATOR_CLASSES = [ProbitVectorGaussian, ProbitVectorUniform, AdditiveVectorUniform,
                          ProbitSingleGaussian, ProbitSingleUniform, AdditiveSingleUniform,
-                        NoMutation]
+                         NoMutation, SingleBeta, VectorDirichlet, SingleGamma, VectorLogMultivariateGamma, AdditiveSingleClipExppGaussian,
+                         AdditiveVectorClipExppGaussian, AdditiveSingleAbsExppGaussian,
+                         AdditiveVectorAbsExppGaussian, AdditiveSingleExppUniform, AdditiveVectorExppUniform,
+                         ArcsSingleClip, ArcsVectorClip]
